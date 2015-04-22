b8d81e55e6892543996f01345b71cd429bc55f9c
MiChAeL <MiChAeL@10.245.207.167>
2015-04-21 23:50:03 -0400

    Creating dialogs

--- src/wofieballdraftkit/gui/EditPlayerDialog.java
+++ src/wofieballdraftkit/gui/EditPlayerDialog.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package wofieballdraftkit.data;
+package wofieballdraftkit.gui;
 
 /**
  *
  * @author MiChAeL
  */
-public class FantasyTeams {
+public class EditPlayerDialog {
     
 }
---
+++ src/wofieballdraftkit/gui/FantasyTeamDialog.java
@@ -0,0 +1,171 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wofieballdraftkit.gui;
+
+import wofieballdraftkit.WBDK_PropertyType;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import properties_manager.PropertiesManager;
+import wofieballdraftkit.data.FantasyTeam;
+import static wofieballdraftkit.gui.WBDK_GUI.CLASS_HEADING_LABEL;
+import static wofieballdraftkit.gui.WBDK_GUI.CLASS_PROMPT_LABEL;
+import static wofieballdraftkit.gui.WBDK_GUI.PRIMARY_STYLE_SHEET;
+
+
+
+/**
+ *
+ * @author MiChAeL
+ */
+public class FantasyTeamDialog extends Stage {
+    FantasyTeam fantasyTeam;
+    
+    // GUI CONTROLS FOR OUR DIALOG
+    GridPane gridPane;
+    Scene dialogScene;
+    Label headingLabel;
+    Label nameLabel;
+    TextField nameTextField;
+    Label ownerLabel;
+    TextField ownerTextField;
+    Button completeButton;
+    Button cancelButton;
+    
+    // THIS IS FOR KEEPING TRACK OF WHICH BUTTON THE USER PRESSED
+    String selection;
+    
+    // CONSTANTS FOR OUR UI
+    public static final String COMPLETE = "Complete";
+    public static final String CANCEL = "Cancel";
+    public static final String NAME_PROMPT = "Name: ";
+    public static final String OWNER_PROMPT = "Owner";
+    public static final String FANTASY_HEADING = "Fantasy Team Details";
+    public static final String ADD_FANTASY_TITLE = "Add New Fantasy Team";
+    
+    /**
+     * Initializes this dialog so that it can be used for either adding
+     * new schedule items or editing existing ones.
+     * 
+     * @param primaryStage The owner of this modal dialog.
+     */
+    public FantasyTeamDialog(Stage primaryStage, FantasyTeam team,  MessageDialog messageDialog) {       
+        // MAKE THIS DIALOG MODAL, MEANING OTHERS WILL WAIT
+        // FOR IT WHEN IT IS DISPLAYED
+        initModality(Modality.WINDOW_MODAL);
+        initOwner(primaryStage);
+        
+        // FIRST OUR CONTAINER
+        gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10, 20, 20, 20));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        
+        // PUT THE HEADING IN THE GRID, NOTE THAT THE TEXT WILL DEPEND
+        // ON WHETHER WE'RE ADDING OR EDITING
+        headingLabel = new Label(FANTASY_HEADING);
+        headingLabel.getStyleClass().add(CLASS_HEADING_LABEL);
+    
+        // NOW THE DESCRIPTION 
+        nameLabel = new Label(NAME_PROMPT);
+        nameLabel.getStyleClass().add(CLASS_PROMPT_LABEL);
+        nameTextField = new TextField();
+        nameTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+          //  fantasyTeam.setDescription(newValue);
+        });
+        
+        // AND THE DATE
+        ownerLabel = new Label(OWNER_PROMPT);
+        ownerLabel.getStyleClass().add(CLASS_PROMPT_LABEL);
+        ownerTextField = new TextField();
+        ownerTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+         
+        });
+        
+        // AND FINALLY, THE BUTTONS
+        completeButton = new Button(COMPLETE);
+        cancelButton = new Button(CANCEL);
+        
+        // REGISTER EVENT HANDLERS FOR OUR BUTTONS
+        EventHandler completeCancelHandler = (EventHandler<ActionEvent>) (ActionEvent ae) -> {
+            Button sourceButton = (Button)ae.getSource();
+            FantasyTeamDialog.this.selection = sourceButton.getText();
+            FantasyTeamDialog.this.hide();
+        };
+        completeButton.setOnAction(completeCancelHandler);
+        cancelButton.setOnAction(completeCancelHandler);
+
+        // NOW LET'S ARRANGE THEM ALL AT ONCE
+        gridPane.add(headingLabel, 0, 0, 2, 1);
+        gridPane.add(nameLabel, 0, 1, 1, 1);
+        gridPane.add(nameTextField, 1, 1, 1, 1);
+        gridPane.add(ownerLabel, 0, 2, 1, 1);
+        gridPane.add(ownerTextField, 1, 2, 1, 1);
+        gridPane.add(completeButton, 0, 3, 1, 1);
+        gridPane.add(cancelButton, 1, 3, 1, 1);
+
+        // AND PUT THE GRID PANE IN THE WINDOW
+        dialogScene = new Scene(gridPane);
+        dialogScene.getStylesheets().add(PRIMARY_STYLE_SHEET);
+        this.setScene(dialogScene);
+    }
+    
+    /**
+     * Accessor method for getting the selection the user made.
+     * 
+     * @return Either YES, NO, or CANCEL, depending on which
+     * button the user selected when this dialog was presented.
+     */
+    public String getSelection() {
+        return selection;
+    }
+    
+    public FantasyTeam getFantasyTeam() { 
+        return fantasyTeam;
+    }
+    
+    /**
+     * This method loads a custom message into the label and
+     * then pops open the dialog.
+     * 
+     * @param message Message to appear inside the dialog.
+     */
+    public FantasyTeam showAddScheduleItemDialog() {
+        // SET THE DIALOG TITLE
+        setTitle(ADD_FANTASY_TITLE);
+        
+        // RESET THE SCHEDULE ITEM OBJECT WITH DEFAULT VALUES
+        fantasyTeam = new FantasyTeam();
+        
+        // LOAD THE UI STUFF
+    //    nameTextField.setText(scheduleItem.getDescription());
+   //     ownerTextField.setText(scheduleItem.getLink());
+        
+        // AND OPEN IT UP
+        this.showAndWait();
+        
+        return fantasyTeam;
+    }
+    
+    public void loadGUIData() {
+        // LOAD THE UI STUFF
+     //   nameTextField.setText(FantasyTeam.getDescription());
+     //   ownerTextField.setText(FantasyTeam.getLink());       
+    }
+    
+    public boolean wasCompleteSelected() {
+        return selection.equals(COMPLETE);
+    }
+    
+
+}
